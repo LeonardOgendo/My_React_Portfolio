d58b6f48a16aa410bc786fc2339a698582a27b23
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-05-23 03:35:53 +0300

    Update mobile responsiveness | Post deployment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
         <div className="footer">
             <p><span style={{ color: '#fff', fontSize: '0.8rem', letterSpacing: '0.5px' }}>Powered by</span> <FaReact className="react-icon"/><span id="react-hero">React</span></p>
             <p><span style={{ color: '#fff', fontSize: '0.8rem' }}>Developed by</span> <span style={{ color: '#f24312', fontSize: '1rem', fontWeight: 'bold', letterSpacing: '0.5px' }}>Leonard Ogendo</span></p>
+            <span id="copyright">© 2025 Leon</span>
         </div>
     )
 };
